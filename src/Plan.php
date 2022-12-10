e93c337141927755b7e51a4634e6a1c69abe1328
Ruan Luies <omega@live.co.za>
2022-12-10 23:26:45 +0200

    Update Plan.php

--- src/Plan.php
+++ src/Plan.php
@@ -2,12 +2,10 @@
 
 namespace FintechSystems\Payfast;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Plan extends Model
 {
-    use HasFactory;
     protected $fillable = ['name', 'recurring_amount', 'initial_amount', 'payfast_frequency'];
 }
 
