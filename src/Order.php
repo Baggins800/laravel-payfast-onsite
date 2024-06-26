d50d4d44de098b4d9e8c3e2b41ace67fdb2fbeed
Ruan Luies <omega@live.co.za>
2024-06-26 23:04:43 +0200

    Update Order.php

--- src/Order.php
+++ src/Order.php
@@ -26,7 +26,7 @@ class Order extends Model
         $newRecord = self::create([
             'billable_id' => Auth::user()->getKey(),
             'billable_type' => Auth::user()->getMorphClass(),
-            'ip_address' => Request::ip()
+            'ip_address' => $_SERVER['REMOTE_ADDR'],
         ]);
 
         return $newRecord->id . '-' . Carbon::now()->format('YmdHis');
